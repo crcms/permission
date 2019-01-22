5dff37d665c0357e0a7983a0a5fa08b63493a304
crcms <crcms@crcms.cn>
2019-01-22 10:05:27 +0800

    init

---
+++ src/Handlers/Menu/StoreHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CrCms\Permission\Handlers\Menu;
+
+use CrCms\Foundation\Handlers\AbstractHandler;
+use CrCms\Foundation\Transporters\Contracts\DataProviderContract;
+
+/**
+ * Class StoreHandler
+ * @package CrCms\Permission\Handlers\Menu
+ */
+class StoreHandler extends AbstractHandler
+{
+    public function handle(DataProviderContract $provider)
+    {
+        
+    }
+}
---
+++ src/Http/Api/Controllers/MenuController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CrCms\Permission\Http\Api\Controllers;
+
+use CrCms\Foundation\Services\ResponseTrait;
+use Illuminate\Routing\Controller;
+
+/**
+ * Class MenuController
+ * @package CrCms\Permission\Http\Api\Controllers
+ */
+class MenuController extends Controller
+{
+    use ResponseTrait;
+
+    public function store()
+    {
+        
+    }
+}
