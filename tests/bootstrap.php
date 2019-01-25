a095c89002c1be34971d35caccdf3744fb89b7d4
crcms <crcms@crcms.cn>
2019-01-25 15:27:47 +0800

    Clear

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,55 +1,202 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2019/1/23
- * Time: 14:34
- */
 
+$config = require __DIR__.'/../config/config.php';
+$login = array_keys($config['login_rules']);
+
+// init app
 $app = new \Illuminate\Container\Container();
 \Illuminate\Container\Container::setInstance($app);
+\Illuminate\Support\Facades\Facade::setFacadeApplication($app);
 
-$app->alias('config',\Illuminate\Contracts\Config\Repository::class);
-$app->alias('config',\Illuminate\Config\Repository::class);
+//alias
+$app->alias('config', \Illuminate\Contracts\Config\Repository::class);
+//$app->alias('config', \Illuminate\Config\Repository::class);
+//$app->alias('queue', \Illuminate\Queue\QueueManager::class);
+$app->alias('queue', \Illuminate\Contracts\Queue\Factory::class);
+$app->alias('queue', \Illuminate\Contracts\Queue\Monitor::class);
+$app->alias('events', \Illuminate\Contracts\Events\Dispatcher::class);
+//$app->alias('events', \Illuminate\Events\Dispatcher::class);
+$app->alias('cache', \Illuminate\Cache\CacheManager::class);
+$app->alias('cache', \Illuminate\Contracts\Cache\Factory::class);
+$app->alias('cache.store', \Illuminate\Cache\Repository::class);
+$app->alias('cache.store',  \Illuminate\Contracts\Cache\Repository::class);
+$app->alias('validator',  \Illuminate\Validation\Factory::class);
+$app->alias('validator',  \Illuminate\Contracts\Validation\Factory::class);
 
-$app->singleton('config', function() {
-
+//config
+$app->singleton('config', function () use ($config) {
     return new \Illuminate\Config\Repository([
+        'app' => [
+            'locale' => 'zh-CN',
+            'fallback_locale' => 'en',
+        ],
+        'passport' => $config,
         'database' => [
-
             'default' => 'mysql',
+            'migrations' => 'migrations',
             'connections' => [
                 'mysql' => [
                     'driver' => 'mysql',
-                    'host' => '127.0.0.1',
+                    'host' => '192.168.1.251',
                     'port' => '3306',
-                    'database' => 'permission',
-                    'username' => 'homestead',
-                    'password' => 'secret',
-
+                    'database' => 'default',
+                    'username' => 'root',
+                    'password' => 'root',
                     'charset' => 'utf8mb4',
                     'collation' => 'utf8mb4_unicode_ci',
                     'prefix' => '',
+                    'prefix_indexes' => true,
                     'strict' => true,
                     'engine' => null,
                 ],
+            ],
+        ],
+        'queue' => [
+            'default' => 'sync',
+            'connections' => [
+
+                'sync' => [
+                    'driver' => 'sync',
+                ],
             ]
+        ],
+        'cache' => [
+
+            'default' => env('CACHE_DRIVER', 'file'),
+
+
+            'stores' => [
+
+                'array' => [
+                    'driver' => 'array',
+                ],
+
+
+                'file' => [
+                    'driver' => 'file',
+                    'path' => __DIR__.'/cache',
+                ],
+
+            ],
+
+
+            'prefix' => 'passport_testing',
+
+
         ]
     ]);
 });
 
-$event = new \Illuminate\Events\EventServiceProvider($app);
-$event->register();
+//$router = Mockery::mock('Illuminate\Support\Facades\Route');
+//$router->shouldReceive('namespace');
+//$router->shouldReceive('register');
+//$router->shouldReceive('group');
+//$router = Mockery::mock('router');
+//$Route = Mockery::mock('Illuminate\Support\Facades\Route');
+//$Route->shouldReceive('namespace','register','group');
 
-$database = new \Illuminate\Database\DatabaseServiceProvider($app);
-$database->register();
-$database->boot();
+$app->instance('path.lang', __DIR__.'./');
 
-//$hash = new \Illuminate\Hashing\HashServiceProvider($app);
-//$hash->register();
+function config_path($path = null)
+{
+    return is_null($path) ? __DIR__ : __DIR__.'/'.$path;
+}
 
-//\Illuminate\Support\Facades\Facade::setFacadeApplication($app);
+function app_path($path = null)
+{
+    return is_null($path) ? __DIR__ : __DIR__.'/'.$path;
+}
+
+function resource_path($path = null)
+{
+    return is_null($path) ? __DIR__ : __DIR__.'/'.$path;
+}
+function app() {
+    return \Illuminate\Container\Container::getInstance();
+}
+
+
+function config($key,$default = null)
+{
+    \Illuminate\Container\Container::getInstance()->make('config')->get($key,$default);
+}
+
+
+function trans($key = null, $replace = [], $locale = null)
+{
+
+    return \Illuminate\Container\Container::getInstance()->make('translator')->trans($key, $replace, $locale);
+}
+
+//$request = Mockery::mock('request');
+//$request->shouldReceive('all')->andReturn([]);
+//
+//$app->instance('request',$request);
+
+
+//service providers
+$providers = [
+    \Illuminate\Database\DatabaseServiceProvider::class,
+    \Illuminate\Hashing\HashServiceProvider::class,
+    \Illuminate\Queue\QueueServiceProvider::class,
+    \Illuminate\Events\EventServiceProvider::class,
+    \Illuminate\Filesystem\FilesystemServiceProvider::class,
+    \Illuminate\Translation\TranslationServiceProvider::class,
+    \Illuminate\Bus\BusServiceProvider::class,
+    \Illuminate\Cache\CacheServiceProvider::class,
+    \Illuminate\Database\MigrationServiceProvider::class,
+    \Illuminate\Validation\ValidationServiceProvider::class,
+    \CrCms\Foundation\Transporters\DataServiceProvider::class,
+    \CrCms\Repository\RepositoryServiceProvider::class,
+    \CrCms\Passport\Providers\PassportServiceProvider::class,
+];
+
+$providers = array_map(function ($provider) use ($app) {
+    return new $provider($app);
+}, $providers);
+
+foreach ($providers as $provider) {
+    $provider->register();
+}
+
+foreach ($providers as $provider) {
+    if (get_class($provider) === \CrCms\Foundation\Transporters\DataServiceProvider::class) {
+        continue;
+    }
+    if (method_exists($provider, 'boot')) {
+        $provider->boot();
+    }
+
+}
+
+
+//\Illuminate\Support\Facades\Artisan::call('migrate:install');
+
+//$app->make(\Illuminate\Database\Migrations\DatabaseMigrationRepository::class)
+//->setSource('sqlite')
+//->createRepository();
+
+//$app->make('migrator')
+//    ->run(__DIR__.'/../database/migrations');
+if (!$app->make('migrator')->getRepository()->repositoryExists()) {
+    $app->make('migrator')->getRepository()->createRepository();
+}
+$app->make('migrator')->reset([__DIR__.'/../database/migrations']);
+$app->make('migrator')->run(__DIR__.'/../database/migrations');
+
+//sleep(5);
+
+$app->make(DatabaseSeeder::class)->run();
+
+
+//$app->make('migrator')->run();
+
+//$factory->define(\CrCms\Passport\Models\UserModel::class, function (\Faker\Generator $faker) {
+//    return [
+//        'name' => $faker->name,
+//        'status' => 1,
+//        'app_id' => 1,
+//    ];
+//});
 
 return $app;
-
-
