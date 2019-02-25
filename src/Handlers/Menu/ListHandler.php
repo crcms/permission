333a9a0004ac1c210ba22b3a5900b83f1bc10af7
qingyueyiyun <qingyueyiyun@163.com>
2019-02-25 16:05:39 +0800

    feat: menu List toTree

--- src/Handlers/Menu/ListHandler.php
+++ src/Handlers/Menu/ListHandler.php
@@ -20,6 +20,6 @@ class ListHandler extends AbstractHandler
         /* @var MenuRepository $repository */
         $repository = $this->app->make(MenuRepository::class);
 
-        return $repository->magic(new MenuMagic($provider->all()))->get();
+        return $repository->magic(new MenuMagic($provider->all()))->get()->toTree();
     }
 }
